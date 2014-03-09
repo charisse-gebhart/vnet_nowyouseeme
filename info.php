3dda84c4ec588e1e8d27fe40fe1d692bdb2b22a3
charisse-gebhart <charisse_gebhart@hotmail.com>
2014-03-09 17:56:25 +1000

    implemented shuffle of images

--- info.php
+++ info.php
@@ -1,28 +1,35 @@
 <?php
-
-// Image Server application for Virtualisation subject
-// Home page - Show thumbnail views of random photo selection 
-// Have multiple uploads enabled
-// @author Charisse Gebhart
-// @date 2014-02-16
-
-//function imgSelect($id) {
-//  $id =  slice(glob("images/*.*"))
-//}
+/**
+* Image Server application for Virtualisation subject
+* Home page - Show thumbnail views of random photo selection 
+* Have multiple uploads enabled
+* @author Charisse Gebhart
+* @date 2014-02-16
+*/
 
 $fileTotal = count(glob("images/*")); // counts total number of image files in the dir so a random selection can be chosen from all images
 $imgArray = glob("images/*");
 
 ?><!DOCTYPE html>
 
-<html>
-<head>
-  <title>Now You See Me! - Your Photos in the Cloud</title>
-  <link rel="stylesheet" type="text/css" href="nysm.css">
-</head>
-<body>
+<html lang="en">
+     <head>
+          <meta charset="UTF-8">
+          <title>Now You See Me! - Your Photos in the Cloud</title>
+          <link rel="stylesheet" type="text/css" href="nysm.css">
+	  <script src="https://code.jquery.com/jquery-git1.min.js"></script>
+	  <script>
+	     $(function() {
+		 // Every time a file input box gets a value (i.e. a file has been attached)
+	  	 $('#uploads').on('change', 'input[type=file]', function() {
+			$(this).after('<br/><input type="file" name="file[]"/>');
+     		 });
+	     });
+     	  </script>
+     </head>
+     <body>
   
-  <h1>Now You See Me</h1>
+         <h1>Now You See Me</h1>
   
   <div class="wrap">
     <div class="box">
@@ -85,11 +92,11 @@ $imgArray = glob("images/*");
   </div> <!--end wrap-->
 
   <div class="upload">
-    <form action="upload_file.php" method="post" enctype="multipart/form-data">
+    <form id="uploads" action="upload_file.php" method="post" enctype="multipart/form-data">
       <label for="file">Upload a New Photo:</label>
-      <input type="file" name="file" id="file" multiple=""><br>
-      <label>Enter a Caption<input type="text" name="title" id="title"></label><br>
-      <input type="submit" name="submit" value="Submit">
+      <input type="file" name="file[]" id="file"><br>
+     <!-- <label>Enter a Caption<input type="text" name="title" id="title"></label><br>-->
+      <input type="submit" name="submit" value="Upload">
     </form>
   </div>
 </body>
--- upload_file.php
+++ upload_file.php
@@ -1,26 +1,44 @@
 <?php
-$file_type = $_FILES["file"]["type"]; // Returns MIME type
-$allowedTypes = array("image/jpg","image/jpeg", "image/png", "image/gif");   // jpg, jpeg, png and gif files only
 
-if(!in_array($file_type, $allowedTypes)) // check if file type is one of the allowed formats
-{
-echo "Only jpg files allowed";
-}
-elseif (file_exists("images/" . $_FILES["file"]["name"])) // check if file already exists
-{
-echo $_FILES["file"]["name"] . " already exists. ";
-}
-elseif ($_FILES["file"]["error"] > 0)  // check for other errors
-{
-echo "Error: " . $_FILES["file"]["error"] . "<br>";
-}
-else
-{
-set_time_limit(300);
+/**
+* Based on Matt Carter's example multiple upload file
+* @author Matt Carter <m@ttcarter.com>
+* @date 2014-02-11
+*/
 
-move_uploaded_file($_FILES["file"]["tmp_name"],"images/" . $_FILES["file"]["name"];
-echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-echo "Type: " . $_FILES["file"]["type"] . "<br>";
-echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-}
-?>
+if ($_FILES && isset($_FILES['file'])) { // when upload occurs
+     foreach ($_FILES['file']['tmp_name'] as $no => $tmp) { // for each file, $no makes the key variable a counter
+	if (!$tmp) 
+	    continue; // must have filename to continue
+	$id = basename($tmp);// extract basic filename without path
+	$id = substr($id,3); // remove php prefix
+
+	if ($_FILES['file']['type'][$no] == 'image/jpeg') {
+	    $ext = 'jpg';    // add the correct file extension 
+	} elseif ($_FILES['file']['type'][$no] == 'image/png') {
+	    $ext = 'png';
+	} else {
+	    echo "Unknown file type<br/>";
+	    continue;
+	}
+
+	//if (file_exists("images/" . $_FILES['file']['name'][$no])) { // check if file already exists
+	//    echo $_FILES['file']['name'] . "already exists. <br/>";
+	//} elseif ($_FILES['file']['error'][$no] > 0) { // check for other errors 
+	//    echo "Error: " . $_FILES['file']['error'] . "<br/>"
+	//} else continue;
+
+  	echo "Saving $id.$ext...<br/>";
+	set_time_limit(300); //extend time limit by 5 minutes to allow larger file uploads
+	move_uploaded_file($tmp,"images/$id.$ext");
+    }
+}?><!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title> Uploaded Files - Now You See Me</title>
+	</head>
+	<body>
+	<p>Thank You</p>	
+	</body>
+</html>
