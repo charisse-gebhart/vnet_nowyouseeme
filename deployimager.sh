709d50868eb1191b6b51edc6c19658c9f12b3e6b
charisse-gebhart <charisse_gebhart@hotmail.com>
2014-04-11 11:10:48 +1000

    final version with no extras

--- deployimager.sh
+++ deployimager.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install apache2 php5 libapache2-mod-php5
-#sudo apt-get -y install imagemagick
 sudo apt-get -y install git
 
 # pull in ISS from github repo
 git clone git@github.com:charisse-gebhart/vnet_nowyouseeme.git && sudo cd ~/git/vnet_nowyouseeme
 
-# create fake host on local machine and set document root "DirectoryIndex info.php" and "DocumentRoot /var/www"
-sudo echo "127.0.0.1 nowyouseeme.com" >> /etc/hosts
+# set "DirectoryIndex info.php" and "DocumentRoot ~/git/vnet_nowyouseeme"
 sudo cp default /etc/apache2/sites-available
 
 # replace php.ini file to allow upload_max_filesize = 100M
@@ -18,7 +16,4 @@ sudo /etc/init.d/apache2 reload
 # mount s3 cloud storage to images folder
 sudo mkdir /var/www/images
 bash s3Bucket.sh
-
-# Update ISS from repo every hour
-bash update_nysm.sh
 
